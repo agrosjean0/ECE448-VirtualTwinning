9b74c97f9b142f6b58f792be5cb7feab64a173cf
Epsilon391 <85028735+Epsilon391@users.noreply.github.com>
2023-10-17 18:12:12 -0400

    Add files via upload

--- Physical Circuit Analysis/test_Fsamp.m
+++ Physical Circuit Analysis/test_Fsamp.m
@@ -9,10 +9,12 @@ t_rise = abs(t_high-t_low);
 vin = 24;     % input DC voltage
 Rdson = 0.1;  % transistor ESR (estimated)
 RL = 0.2;     % inductor ESR (estimated)
-RC = 0.01+0.12;    % capacitor ESR (estimated)
+RC_det = 0.12; % increases pk to pk amp & decreases roundness
+RC = 0.01+RC_det;    % capacitor ESR (estimated)
 R = 10.9;       % load resistance
 L = 0.0001;   % inductance
-C = 0.0001-0.0000;   % capacitance
+C_det = 0.0000; % increases pk to pk amp & roundness
+C = 0.0001-C_det;   % capacitance
 VF = 0.7;     % diode forward voltage
 
 % initial cicuit AC conditions
@@ -21,34 +23,36 @@ vC = 0;       % initial capacitor voltage
 
 % switch waveform
 fsw = 50000;  % 50 kHz switching freq
-Tstep = 1E-8; % 10 ns dt iteration step
+fsamp = 100E6; % sample rate of 100 MHz
+Tstep = 1/fsamp; % dt iteration step
 D = 0.5;      % duty cycle
 
 %% Switch
 Tsw = 1/fsw;
-t = 0:Tstep:0.1-Tstep;
+sim_time = 0.1;
+t = 0:Tstep:sim_time-Tstep;
 t_per = 0:Tstep:Tsw-Tstep;
-S = mod(t,Tsw)>Tsw*D;
+S = mod(t,Tsw)>Tsw*D; % PWM waveform
 % S1 = t_per<(D*Tsw-t_rise/2);
 % S2 = t_per>(D*Tsw+t_rise/2);
 % t(t_per>(D*Tsw+t_rise/2)) = 1;
+
 %% Circuit analysis
 vals = zeros([3 size(S,2)]);
 vo = zeros([1 size(S,2)]);
 
+  % switch independent component
+  A = S.*Rdson + RL + RC*R/(RC + R);
+  Se = [-A(1)./L (-1/L*(R/(RC+R)));
+      (1/C*(R/(RC+R))) -(1/C*(1/(RC+R)));
+      (RC*R/(RC+R)) R/(RC+R)];
+  % switch dependent components
+  Son = S.*[vin/L;0;0];
+  Soff = (1-S).*[-VF/L;0;0];
 for itr = 1:size(S,2)
-    % switch independent component
-    A = S(itr).*Rdson + RL + RC*R/(RC + R);
-    Se = [-A./L (-1/L*(R/(RC+R)));
-        (1/C*(R/(RC+R))) -(1/C*(1/(RC+R)));
-        (RC*R/(RC+R)) R/(RC+R)];
-
-    % switch dependent components
-    Son = S(itr).*[vin/L;0;0];
-    Soff = (1-S(itr)).*[-VF/L;0;0];
-    
+    Se(1) = -A(itr)/L;
     % calculate change 
-    vals = Se*[iL;vC] + Son + Soff;
+    vals = Se*[iL;vC] + Son(:,itr) + Soff(:,itr);
     iL = Tstep*vals(1) + iL;
     vC = Tstep*vals(2) + vC;
     vo(itr) = vals(3);
@@ -75,7 +79,7 @@ ylabel('Voltage (V)')
 
 %% Plot vc Physical
 ps = 0;
-vo_samp = vo(1,(ps)+9E6:(ps)+9E6+1E4);
+vo_samp = vo(1,(ps)+(sim_time*1/Tstep)-2E4:(ps)+round(sim_time*1/Tstep-1E4));
 
 figure
 Tvc = table2array(readtable('V2_DC.csv'));
